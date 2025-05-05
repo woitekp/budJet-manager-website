3d3b40827e4c5848c4af9e6ec64dd33450dde1ec
wpodbielski <wojtek.podbielski.programmer@gmail.com>
2025-05-05 21:49:43 +0200

    categories form refactoring

--- src/App/views/settings/create.php
+++ src/App/views/settings/create.php
@@ -6,14 +6,12 @@ $inputName = escape($inputName);
 <tr class="<?php echo $rowStyle ?>">
   <th class="null width-ge-50"></th>
   <form method="post">
+    <?php include $this->resolve("partials/_csrf.php") ?>
     <th class="null width-ge-180">
       <label for=<?php echo $inputName ?> class="sr-only">Category</label>
       <input name=<?php echo $inputName ?> type="text" min="0" step="0.01" class="form-control text-centered" placeholder="Category">
     </th>
     <th class="null">
-
-      <?php include $this->resolve("partials/_csrf.php") ?>
-
       <button name="category" class="confirm th-button btn width-ge-80" type="submit">
         Add
       </button>
