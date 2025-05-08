301aec66c0e524885ac9cf5eb29122bd9cbcfec5
wpodbielski <wojtek.podbielski.programmer@gmail.com>
2025-05-08 20:44:04 +0200

    settings empty categories corrected

--- src/App/views/settings/categories.php
+++ src/App/views/settings/categories.php
@@ -8,11 +8,6 @@
       <th>#</th>
       <th>Name</th>
     </tr>
-  <?php else: ?>
-    <tr class="column_names <?php echo $rowStyle ?>">
-      <th class="null"></th>
-      <th class="null"></th>
-    </tr>
   <?php endif; ?>
 
   <?php
