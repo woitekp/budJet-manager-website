443658517a582ec5c5a4664cc1cab795362f0c61
wpodbielski <wojtek.podbielski.programmer@gmail.com>
2025-05-29 20:12:04 +0200

    paths to assets in views html header fixed

--- src/App/views/partials/_header.php
+++ src/App/views/partials/_header.php
@@ -7,8 +7,8 @@
   <title><?php echo escape($title); ?> - BudJet Manager</title>
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/main.css">
-  <link rel="icon" href="assets/logo.svg" sizes="any" type="image/svg+xml">
+  <link rel="stylesheet" href="/assets/main.css">
+  <link rel="icon" href="/assets/logo.svg" sizes="any" type="image/svg+xml">
 </head>
 
 <body>
@@ -17,7 +17,7 @@
 
     <h1>
       <img
-        src="assets/logo.svg"
+        src="/assets/logo.svg"
         height="90"
         width="90" />
       <a href="/balance" class="logo">BudJet Manager</a>
